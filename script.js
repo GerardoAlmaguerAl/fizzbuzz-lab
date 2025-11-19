f1e288a0e1b2502e311a75adf739540b412a574e
GerardoAlmaguer <gerardoalmagueral@gmail.com>
2025-11-19 11:23:17 -0600

    feat: Modificacion de Archivos index.html y definicion de funcion fizzbuzz en archivo script.js

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
 //Here it is defined the fizzBuzz function,that returns  the fizz Value
 
+console.log("Starting FizzBuzz");
+let fizzcount = 0;
+let buzzcount = 0;
+let fizzbuzzcount = 0;
+
 function fizzBuzz() {
-  for (let i = 1; i <= 1000; i++) {
+  for (let i = 1; i <= 1000; i++) {                          
     if (i % 3 == 0  && i % 5 == 0 ) {
       console.log("FizzBuzz");
+      fizzbuzzcount++;
     } else if (i % 3 == 0) {
       console.log("Fizz");
+      fizzcount++;
     } else if (i % 5 == 0) {
       console.log("Buzz");
+      buzzcount++;
     } else {
       console.log(i);
     }
   }
+console.log("Total Fizz: " + fizzcount);  
+console.log("Total Buzz: " + buzzcount);  
+console.log("Total FizzBuzz: " + fizzbuzzcount);
 }
+
+
