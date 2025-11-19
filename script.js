7a691d163c6debab507a79c6b70096d974b9082a
GerardoAlmaguer <gerardoalmagueral@gmail.com>
2025-11-19 10:35:26 -0600

    feat: Modificacion de Archivo script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+//Here it is defined the fizzBuzz function,that returns  the fizz Value
+
+function fizzBuzz() {
+  for (let i = 1; i <= 1000; i++) {
+    if (i % 3 == 0  && i % 5 == 0 ) {
+      console.log("FizzBuzz");
+    } else if (i % 3 == 0) {
+      console.log("Fizz");
+    } else if (i % 5 == 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
+  }
+}
