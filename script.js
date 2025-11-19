8145f2605d700e70710a8e69b5b0e31c34a741f8
GerardoAlmaguer <gerardoalmagueral@gmail.com>
2025-11-19 17:07:38 -0600

    feat: Modificacion de archivo script.js

--- script.js
+++ script.js
@@ -1,28 +1,29 @@
 //Here it is defined the fizzBuzz function,that returns  the fizz Value
 
 console.log("Starting FizzBuzz");
-let fizzcount = 0;
-let buzzcount = 0;
-let fizzbuzzcount = 0;
+//let fizzcount = 0;
+//let buzzcount = 0;
+//let fizzbuzzcount = 0;
 
 function fizzBuzz() {
   for (let i = 1; i <= 1000; i++) {                          
     if (i % 3 == 0  && i % 5 == 0 ) {
       console.log("FizzBuzz");
-      fizzbuzzcount++;
+    //  fizzbuzzcount++;
     } else if (i % 3 == 0) {
       console.log("Fizz");
-      fizzcount++;
+     // fizzcount++;
     } else if (i % 5 == 0) {
       console.log("Buzz");
-      buzzcount++;
+     // buzzcount++;
     } else {
       console.log(i);
     }
   }
-console.log("Total Fizz: " + fizzcount);  
-console.log("Total Buzz: " + buzzcount);  
-console.log("Total FizzBuzz: " + fizzbuzzcount);
+//console.log("Total Fizz: " + fizzcount);  
+//console.log("Total Buzz: " + buzzcount);  
+//console.log("Total FizzBuzz: " + fizzbuzzcount);
 }
 
+//fizzBuzz();
 
